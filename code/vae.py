9047299a45f7b87f0c9d838bfeebc42e044a1986
Garethlomax <gcl15@ic.ac.uk>
2020-04-26 17:37:01 +0100

    added forward encode decode latent space to vae

--- code/vae.py
+++ code/vae.py
@@ -5,7 +5,7 @@ Created on Wed Apr  8 14:10:46 2020
 
 @author: garethlomax
 """
-
+import torch
 import torch.nn as nn 
 
 class VAE(nn.module):
@@ -48,11 +48,67 @@ class VAE(nn.module):
         self.il4 = nn.ConvTranspose1d(in_channels = 16, out_channels = 8, kernel_size = 16, paddin = 1, stride = 4)
         self.il5 = nn.ConvTranspose1d(in_channels = 8, out_channels = 1, kernel_size = 32, padding = 1, stride = 3)
         
-        self.bn5 = nn.BatchNorm1d(8)
-        self.bn4 = nn.BatchNorm1d(16)
-        self.bn3 = nn.BatchNorm1d(32)
-        self.bn2 = nn.BatchNorm1d(64)
-        self.bn1 = nn.BatchNorm1d(128)
+        self.ibn5 = nn.BatchNorm1d(8)
+        self.ibn4 = nn.BatchNorm1d(16)
+        self.ibn3 = nn.BatchNorm1d(32)
+        self.ibn2 = nn.BatchNorm1d(64)
+        self.ibn1 = nn.BatchNorm1d(128)
+        
+        self.activation = nn.ReLU()
+        self.sigmoid = nn.Sigmoid()
+        
+    def encoder(self, x):
+        h = self.bn1(self.activation(self.l1(x)))
+        h = self.bn2(self.activation(self.l2(h)))
+        h = self.bn3(self.activation(self.l3(h)))
+        h = self.bn4(self.activation(self.l4(h)))
+        h = self.bn5(self.activation(self.l5(h)))
+        
+        #reshape here
+        h = h.view(-1, 512)
+        
+        h = self.activation(self.fc1(h))
+        h = self.activation(self.fc2(h))
+        
+        logvar = self.fc_logvar(h)
+        mu = self.fc_mu(h)
+        
+        return mu, logvar
+
+    def latent_space(self, mu, log_var):
+        if self.training:
+          std = torch.exp(0.5*log_var)
+          eps = torch.randn_like(std).to(device)
+          z = eps.mul(std).add_(mu) 
+        else:
+          z = mu
+        return z
+        
+    def decoder(self, z):
+        
+        h = self.activation(self.ifc1(z))
+        h = self.activation(self.ifc2(h))
+        h = self.activation(self.ifc3(h))
+        
+        # TODO: check
+        h = self.ibn1(h.view(-1,128,4))
+        
+        
+        h = self.bn2(self.activation(self.il1(h)))
+        h = self.bn3(self.activation(self.il2(h)))
+        h = self.bn4(self.activation(self.il3(h)))
+        h = self.bn5(self.activation(self.il4(h)))
+        
+        reconstruction = self.sigmoid(self.il5(h))
+        
+        return reconstruction
+    
+    def forward(self, x):
+        mu, log_var = self.encoder(x)
+        z = self.latent_space(mu, log_var)
+        reconstruction = self.decoder(z)
+        return reconstruction, z, mu, log_var
+
         
         
         
